bddacf5a5499dc293d6d1540b3caacfbe7c38467
Julie Hache <elektrophile@gmail.com>
2014-08-25 11:48:50 -0400

    Update hello_spec.rb to use new syntax

--- 00_hello/hello_spec.rb
+++ 00_hello/hello_spec.rb
@@ -1,5 +1,5 @@
-# You should be in the folder 00_hello on the command line, 
-# you should have already entered 
+# You should be in the folder 00_hello on the command line,
+# you should have already entered
 # $ gem install rspec
 # and you should be running this problem's tests by entering
 # $ rake
@@ -9,7 +9,7 @@
 # it means get a file with the name "hello.rb" in the current directory.
 require "hello.rb"
 # To stop this error from being thrown we need to create the file:
-# $ touch hello.rb 
+# $ touch hello.rb
 
 # Now if we run rake again, it will complain about "a undefined local variable or method `hello'"
 # Let's keep this in mind while we go through the next little bit of code.
@@ -18,17 +18,17 @@ require "hello.rb"
 describe "the hello function" do
   # neither is #20,
   it "says hello" do
-    # but this is the important part. 
+    # but this is the important part.
     # Line #24 is saying: if we call the method hello, what it returns should be equal to "Hello!"
     # If that's not the case, this is a failing test.
-    hello.should == "Hello!"
+    expect(hello).to eq("Hello!")
     # So in hello.rb let's define a method named hello,
     # and let's make it return "Hello!"
 
     # In hello.rb:
-    # 
+    #
     # def hello
-    #   return "Hello!"
+    #   "Hello!"
     # end
   end
 end
@@ -39,11 +39,11 @@ end
 
 describe "the greet function" do
   it "says hello to someone" do
-    greet("Alice").should == "Hello, Alice!"
+    expect(greet("Alice")).to eq("Hello, Alice!")
   end
 
   it "says hello to someone else" do
-    greet("Bob").should == "Hello, Bob!"
+    expect(greet("Bob")).to eq("Hello, Bob!")
   end
 
   # These two tests are saying there should be a method named greet
