291f52806d513e2a1193b0d275317f8c963082e6
Julie Hache <elektrophile@gmail.com>
2014-08-25 12:40:26 -0400

    Update rpn_calculator_spec.rb to new Rspec syntax

--- bonus_rpn_calculator/rpn_calculator_spec.rb
+++ bonus_rpn_calculator/rpn_calculator_spec.rb
@@ -1,5 +1,5 @@
-# "RPN" stands for "Reverse Polish Notation". 
-# (See the wikipedia entry: http://en.wikipedia.org/wiki/Reverse_Polish_notation) 
+# "RPN" stands for "Reverse Polish Notation".
+# (See the wikipedia entry: http://en.wikipedia.org/wiki/Reverse_Polish_notation)
 # Briefly, in an RPN world, instead of using normal "infix" notation, e.g.
 #
 #     2 + 2
@@ -8,9 +8,9 @@
 #
 #     2 2 +
 #
-# While this may seem bizarre, there are some advantages to doing things this way. 
-# For one, you never need to use parentheses, since there is never any ambiguity 
-# as to what order to perform operations in. The rule is, you always go from the back, 
+# While this may seem bizarre, there are some advantages to doing things this way.
+# For one, you never need to use parentheses, since there is never any ambiguity
+# as to what order to perform operations in. The rule is, you always go from the back,
 # or the left side.
 #
 #     1 + 2 * 3 =>
@@ -20,8 +20,8 @@
 #     1 2 + 3 * => (1 + 2) * 3
 #     1 2 3 * + => 1 + (2 * 3)
 #
-# Another advantage is that you can represent any mathematical formula 
-# using a simple and elegant data structure, called a stack 
+# Another advantage is that you can represent any mathematical formula
+# using a simple and elegant data structure, called a stack
 # (http://en.wikipedia.org/wiki/Stack_(data_structure)).
 #
 # Hint:
@@ -33,17 +33,13 @@ require "rpn_calculator"
 
 describe RPNCalculator do
 
-  attr_accessor :calculator
-
-  before do
-    @calculator = RPNCalculator.new
-  end
+  let!(:calculator) {RPNCalculator.new}
 
   it "adds two numbers" do
     calculator.push(2)
     calculator.push(3)
     calculator.plus
-    calculator.value.should == 5
+    expect(calculator.value).to eq(5)
   end
 
   it "adds three numbers" do
@@ -51,16 +47,16 @@ describe RPNCalculator do
     calculator.push(3)
     calculator.push(4)
     calculator.plus
-    calculator.value.should == 7
+    expect(calculator.value).to eq(7)
     calculator.plus
-    calculator.value.should == 9
+    expect(calculator.value).to eq(9)
   end
 
   it "subtracts the second number from the first number" do
     calculator.push(2)
     calculator.push(3)
     calculator.minus
-    calculator.value.should == -1
+    expect(calculator.value).to eq(-1)
   end
 
   it "adds and subtracts" do
@@ -68,9 +64,9 @@ describe RPNCalculator do
     calculator.push(3)
     calculator.push(4)
     calculator.minus
-    calculator.value.should == -1
+    expect(calculator.value).to eq(-1)
     calculator.plus
-    calculator.value.should == 1
+    expect(calculator.value).to eq(1)
   end
 
   it "multiplies and divides" do
@@ -78,9 +74,9 @@ describe RPNCalculator do
     calculator.push(3)
     calculator.push(4)
     calculator.divide
-    calculator.value.should == (3.0 / 4.0)
+    expect(calculator.value).to eq((3.0 / 4.0))
     calculator.times
-    calculator.value.should == 2.0 * (3.0 / 4.0)
+    expect(calculator.value).to eq(2.0 * (3.0 / 4.0))
   end
 
   it "resolves operator precedence unambiguously" do
@@ -90,7 +86,7 @@ describe RPNCalculator do
     calculator.plus
     calculator.push(3)
     calculator.times
-    calculator.value.should == (1+2)*3
+    expect(calculator.value).to eq((1+2)*3)
 
     # 1 2 3 * + => 1 + (2 * 3)
     calculator.push(1)
@@ -98,7 +94,7 @@ describe RPNCalculator do
     calculator.push(3)
     calculator.times
     calculator.plus
-    calculator.value.should == 1+(2*3)
+    expect(calculator.value).to eq(1+(2*3))
   end
 
   it "fails informatively when there's not enough values stacked away" do
@@ -121,23 +117,18 @@ describe RPNCalculator do
 
   # Extra Credit:
   it "tokenizes a string" do
-    calculator.tokens("1 2 3 * + 4 5 - /").should ==
-      [1, 2, 3, :*, :+, 4, 5, :-, :/]
+    expect(calculator.tokens("1 2 3 * + 4 5 - /")).to eq([1, 2, 3, :*, :+, 4, 5, :-, :/])
   end
 
   # Extra Credit:
   it "evaluates a string" do
-    calculator.evaluate("1 2 3 * +").should ==
-      ((2 * 3) + 1)
+    expect(calculator.evaluate("1 2 3 * +")).to eq(((2 * 3) + 1))
 
-    calculator.evaluate("4 5 -").should ==
-      (4 - 5)
+    expect(calculator.evaluate("4 5 -")).to eq((4 - 5))
 
-    calculator.evaluate("2 3 /").should ==
-      (2.0 / 3.0)
+    expect(calculator.evaluate("2 3 /")).to eq((2.0 / 3.0))
 
-    calculator.evaluate("1 2 3 * + 4 5 - /").should ==
-      (1.0 + (2 * 3)) / (4 - 5)
+    expect(calculator.evaluate("1 2 3 * + 4 5 - /")).to eq((1.0 + (2 * 3)) / (4 - 5))
   end
 
 end
