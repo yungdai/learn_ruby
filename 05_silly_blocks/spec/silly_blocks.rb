96ef54ca4f2376f697dc94b66e9482661c4c37e1
Yung Dai <yungchidai@gmail.com>
2014-11-04 07:47:48 -0500

    adder isn’t working, but my reverser doesn’t quite pass the test

--- 05_silly_blocks/spec/silly_blocks.rb
+++ 05_silly_blocks/spec/silly_blocks.rb
@@ -1,10 +1,17 @@
 def reverser(sentence)
-  reversed = (sentence.split.map do |word|
+  sentence = (sentence.split.map do |word|
     word.reverse
   end).join(' ')
+  puts sentence
 end
 
 
 puts reverser("hello")
 puts reverser("hello dolly")
 
+def adder(number)
+  new_total = number + 5
+  puts new_total
+end
+
+adder(3)
